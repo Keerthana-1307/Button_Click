4361842a3f95a72c5b515b68f6c76176e2b5a399
Keerthana-1307 <keerthana.27csa@licet.ac.in>
2026-04-28 21:58:33 +0530

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,24 @@
+//first go to app/src/main/java/mainActivity.java//
+
+
+package com.example.buttonclick;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+public class MainActivity extends AppCompatActivity {
+   Button btn;
+   @Override
+   protected void onCreate(Bundle savedInstanceState) {
+       super.onCreate(savedInstanceState);
+       setContentView(R.layout.activity_main);
+       btn = findViewById(R.id.btnClick);
+       btn.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               Toast.makeText(MainActivity.this,
+                       "Button Clicked",
+                       Toast.LENGTH_SHORT).show();}});}}
+
+
